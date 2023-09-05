f66ac56f8c221bc27f4b9ee9e6393b27dc7b7bd0
Cristopher Kiri <4031966+cris-kiri@users.noreply.github.com>
2023-09-05 21:19:21 +1200

    refactor: move authorisation from client to server

--- client/pages/friend-wishlists.tsx
+++ client/pages/friend-wishlists.tsx
@@ -1,70 +1,35 @@
-import {
-  getFriendsWishlists,
-  getAuthIdByUserId,
-} from '../apis/get-friends-wishlists'
+import getFriendsWishlists from '../apis/get-friend-wishlists'
 import { useQuery } from '@tanstack/react-query'
 import { Link, useParams } from 'react-router-dom'
+import { FriendWishlist } from '../../models/wishlist'
 
 export default function FriendWishlists() {
-  const { userId } = useParams()
-  const {
-    data: authId,
-    isLoading: userIdIsLoading,
-    error: userIdError,
-  } = useQuery(['users', userId], () => getAuthIdByUserId(Number(userId)))
-
-  console.log(authId)
+  const { friendId } = useParams()
 
   const {
-    data: friendWishlist,
+    data: friendWishlists,
     isLoading,
     error,
-  } = useQuery(['wishlists', 'users', authId], () =>
-    getFriendsWishlists(authId)
+  } = useQuery(['wishlists', 'users', 'friend', friendId], () =>
+    getFriendsWishlists(friendId || '1')
   )
 
-  console.log(friendWishlist)
+  if (error instanceof Error)
+    return <p>{error.message} Error fetching friends wishlists!</p>
 
-  if (error instanceof Error) {
-    return (
-      <>
-        <p>Something went wrong ... {error.message}</p>
-      </>
-    )
-  }
+  if (isLoading) return <p>Loading ...</p>
 
-  if (isLoading) {
-    return (
-      <>
-        <p>Loading ...</p>
-      </>
-    )
-  }
-
-  if (userIdError instanceof Error) {
-    return (
-      <>
-        <p>Something went wrong ... {userIdError.message}</p>
-      </>
-    )
-  }
-
-  if (userIdIsLoading) {
-    return (
-      <>
-        <p>Loading ...</p>
-      </>
-    )
-  }
+  // TODO: Get friend details to display on page
 
   return (
     <>
-      <h2>Friend Wishlists</h2>
-
-      {friendWishlist.map((x) => (
+      <h2>Friends Wishlists</h2>
+      {friendWishlists.map((wishlist: FriendWishlist) => (
         <>
-          <Link to={`/${x.wishlist_id}`}>{x.name} </Link>
-          <h1>{x.description} </h1>
+          <Link key={wishlist.wishlist_id} to={`/${wishlist.wishlist_id}`}>
+            {wishlist.name}
+          </Link>
+          <h3 key={wishlist.description}>{wishlist.description}</h3>
         </>
       ))}
     </>
