31f1c510d85e2dbe75df39eda8969be66a66fde8
Nathalia Prado <nathalia_898@hotmail.com>
2023-09-06 10:52:22 +1200

    Remove mocked data

--- client/pages/home.tsx
+++ client/pages/home.tsx
@@ -5,13 +5,12 @@ import { FriendsWishlist } from '../components/friends-wishlist'
 
 export default function Home() {
 
-  /*
   const { getAccessTokenSilently } = useAuth0()
 
   const { data: friendsWishlist, isLoading, isError } = useQuery(['friendsWishlists'], async () => {
-    const token = await getAccessTokenSilently()
-    console.log(token, 'token')
-    return getWishlists({ token })
+    // TODO After adding token, this line should be uncommented 
+    // const token = await getAccessTokenSilently()
+    return getWishlists({ token: '123' })
   })
 
   console.log(friendsWishlist)
@@ -23,74 +22,6 @@ export default function Home() {
   if (isLoading) {
     return <div>Loading...</div>
   }
-  */
-
-  // MOCKED DATA WORKING
-  const friendsWishlist = [
-      {
-          "friendId": 3,
-          "description": "We are getting married and these are the things we need for our new home!",
-          "name": "Wedding Registry",
-          "wishlistId": 3,
-          "userId": 1,
-          "username": "bobsmith",
-          "fullName": "Bob Smith"
-      },
-      {
-          "friendId": 2,
-          "description": "I have been good this year and these are the things I want for Christmas!",
-          "name": "Christmas Wishlist",
-          "wishlistId": 2,
-          "userId": 1,
-          "username": "janedoe",
-          "fullName": "Jane Doe"
-      },
-      {
-        "friendId": 2,
-        "description": "Party whishlist for my team!",
-        "name": "Party Wishlist",
-        "wishlistId": 3,
-        "userId": 1,
-        "username": "janedoe",
-        "fullName": "Jane Doe"
-      },
-      {
-        "friendId": 2,
-        "description": "Groceries whishlist description!",
-        "name": "Groceries Wishlist",
-        "wishlistId": 4,
-        "userId": 1,
-        "username": "janedoe",
-        "fullName": "Jane Doe"
-    },
-    {
-      "friendId": 2,
-      "description": "Bargain whishlist description!",
-      "name": "Bargain Wishlist",
-      "wishlistId": 5,
-      "userId": 1,
-      "username": "janedoe",
-      "fullName": "Jane Doe"
-    },
-    {
-      "friendId": 2,
-      "description": "Gym whishlist description!",
-      "name": "Gym Wishlist",
-      "wishlistId": 6,
-      "userId": 1,
-      "username": "janedoe",
-      "fullName": "Jane Doe"
-    },
-    {
-      "friendId": 2,
-      "description": "Petshop whishlist description!",
-      "name": "Petshop Wishlist",
-      "wishlistId": 7,
-      "userId": 1,
-      "username": "janedoe",
-      "fullName": "Jane Doe"
-    }
-]
 
   return (
     <>
