d4b0a530930bdb15e5ef88f09b5f654d56db2a24
Cristopher Kiri <4031966+cris-kiri@users.noreply.github.com>
2023-09-05 21:27:41 +1200

    chore: add todo for new routes file

--- server/routes/wishlists.ts
+++ server/routes/wishlists.ts
@@ -31,6 +31,8 @@ router.get('/:auth0_id', async (req, res) => {
 
 // GET /api/v1/wishlists/friends/:friendId
 router.get('/friends/:friendId', async (req, res) => {
+  // TODO: add new routes file for user information rendering this obsolete
+
   try {
     const friendId = req.params.friendId
     const { auth0_id: authId } = await getAuthId(friendId)
