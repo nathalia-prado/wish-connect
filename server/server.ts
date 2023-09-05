4f75831885954962e545b38c4dd980e3d781e5f4
Cristopher Kiri <4031966+cris-kiri@users.noreply.github.com>
2023-09-06 10:01:05 +1200

    feat: fetching friend details

--- server/server.ts
+++ server/server.ts
@@ -3,10 +3,12 @@ import * as Path from 'node:path'
 import express from 'express'
 
 import wishListRouter from './routes/wishlists'
+import userDetailsRouter from './routes/user-details'
 
 const server = express()
 server.use(express.json())
 server.use('/api/v1/wishlists', wishListRouter)
+server.use('/api/v1/user-details', userDetailsRouter)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
