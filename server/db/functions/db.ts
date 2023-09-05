52db9cff608f6f16a63ed447d2ace952961a38ca
popoxta <popoxta@gmail.com>
2023-09-05 14:45:52 +1200

    Reset files as per instructions

--- server/db/functions/db.ts
+++ server/db/functions/db.ts
@@ -1,24 +1,3 @@
 import connection from '../connection'
 
-export async function getUserFriendsWishlist(
-  auth0_id: string,
-  db = connection
-) {
-  const friendsWishlists = await db('friends')
-    .join('users', 'friends.user_id', 'users.id')
-    .join('wishlist', 'friends.friend_id', 'wishlist.user_id')
-    .where('users.auth0_id', auth0_id).where('wishlist.private', 0)
-    .select(
-      'users.id',
-      'users.auth0_id',
-      'friends.user_id',
-      'friends.friend_id',
-      'wishlist.id as wishlist_id',
-      'wishlist.name',
-      'wishlist.description',
-      'wishlist.user_id as wishlist_user_id',
-      'wishlist.private'
-    )
 
-  return friendsWishlists
-}
--- server/routes/wishlists.ts
+++ server/routes/wishlists.ts
@@ -9,22 +9,6 @@ import { checkJwt } from '../utils/auth'
 const router = express.Router()
 
 // GET /api/v1/wishlists
-router.get('/:auth0_id', async (req, res) => {
-  //Get all wishlists for all friends of a user
-  const auth0_id = req.params.auth0_id
 
-  try {
-    const wishlists = await getUserFriendsWishlist(auth0_id)
-    console.log(wishlists)
-    //deconstructs the body of the response and
-
-    res.json(wishlists)
-  } catch (err) {
-    console.log(err)
-    res.status(500).json({ message: 'Internal server error' })
-  }
-})
-
-//
 
 export default router
