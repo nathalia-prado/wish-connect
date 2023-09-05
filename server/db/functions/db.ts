0ac35c10779bc3e56d05b3ab437757ff11c686be
popoxta <popoxta@gmail.com>
2023-09-05 15:53:02 +1200

    Add method to return friends details and wishlist

--- server/db/functions/db.ts
+++ server/db/functions/db.ts
@@ -1,3 +1,23 @@
 import connection from '../connection'
 
+function getFriendsWishlistsByAuthId(id: string, db = connection) {
+  const subQuery =  connection('users AS u')
+    .join('friends AS f', 'u.id', '=', 'f.user_id')
+    .join('wishlist AS w', 'w.user_id', '=', 'u.id')
+    .select( 'w.user_id AS friendId',
+      'w.description',
+      'w.name',
+      'w.id AS wishlistId',
+      'f.friend_id AS userId',
+      'u.username',
+      'u.full_name AS fullName'
+    )
+    .as('s')
 
+
+  return db('users AS u').select('s.*')
+    .join(subQuery, 's.userId', '=', 'u.id')
+    .where('u.auth0_id', id)
+}
+
+export {getFriendsWishlistsByAuthId}
--- server/routes/wishlists.ts
+++ server/routes/wishlists.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getUserFriendsWishlist } from '../db/functions/db'
+import { getFriendsWishlistsByAuthId } from '../db/functions/db'
 
 import { User } from '../../models/user'
 import { Wishlist } from '../../models/wishlist'
@@ -10,5 +10,17 @@ const router = express.Router()
 
 // GET /api/v1/wishlists
 
+//todo remove hardcoded auth ID and replace with token
+router.get('/friends', async (req, res, next) => {
+  try {
+    const userId = 'auth0|123456'
+    const wishlists = await getFriendsWishlistsByAuthId(userId)
+
+    res.json(wishlists)
+
+  } catch (e) {
+    console.log(`An error has occurred at ${req.path} - ${e}`)
+  }
+})
 
 export default router
