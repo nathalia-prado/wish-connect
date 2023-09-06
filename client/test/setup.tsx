cb820110416869b43a38c0310d55f44bdacc5d02
David Gutierrez <thedoctorguro@gmail.com>
2023-09-06 16:27:47 +1200

    Adds jsdom, express-jwt, jsonwebtoken and jwks-rsa to the dependencies (updating package.json). Ads a cheeky button to the nav-bar that allows the usre to reveal the token to the console (dubious usefulness, try for fun). Adds functionalities to the setup.tsx file to allow more testability to the frontend. Updates seed files to include hardcoded ids as suggested by Gabriel the Wise and Lari the Undefined. Updates the routes with the checkJwt middleware on the original GET route (only for testing purposes). Updates the auth.ts file to improve its functionality with the JwtRequest interface and other goodies in the checkJwt middleware function. Done with love, patience and a bit of madness. Jah.

--- client/test/setup.tsx
+++ client/test/setup.tsx
@@ -1,8 +1,13 @@
 import matchers, {
   TestingLibraryMatchers,
 } from '@testing-library/jest-dom/matchers'
-import { cleanup } from '@testing-library/react'
+import { cleanup, render } from '@testing-library/react'
 import { beforeEach, expect } from 'vitest'
+import userEvent from '@testing-library/user-event'
+import { RouterProvider, createMemoryRouter } from 'react-router-dom'
+import { routes } from '../routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import '@testing-library/jest-dom/vitest'
 
 beforeEach(cleanup)
 expect.extend(matchers)
@@ -14,4 +19,32 @@ declare global {
     interface Matchers<R = void, T = {}>
       extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
   }
+}
+
+export function renderRoute(location: string) {
+  const router = createMemoryRouter(routes, {
+    initialEntries: [location],
+  })
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+    logger: {
+      log: console.log,
+      warn: console.warn,
+      error: () => {},
+    },
+  })
+
+  const user = userEvent.setup()
+  const screen = render(
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  )
+
+  return { user, ...screen }
 }
