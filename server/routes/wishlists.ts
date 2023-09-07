26fa08b9c0078e813e24bac6316119b980dd5a84
Zoe Proudfoot <zoeannproudfoot123@gmail.com>
2023-09-07 15:02:58 +1200

    I have no message.

--- server/routes/wishlists.ts
+++ server/routes/wishlists.ts
@@ -1,21 +1,18 @@
 import express from 'express'
-<<<<<<< HEAD
-import { getUserFriendsWishlists } from '../db/functions/db'
+import {} from '../db/functions/db'
 import { User } from '../../models/user'
 import { Wishlist } from '../../models/wishlist'
-import { checkJwt } from '../utils/auth'
 import { addWishlist, editWishlist } from '../db/wishlist'
 // import { addWishlist } from '../db/wishlist'
-=======
 import {
   getAuthId,
   getFriendDetails, //Make sure to import only necessary functions to the correct routes
   getUserFriendsWishlist,
+  getUserFriendsWishlists,
 } from '../db/functions/db'
 
 import { JwtRequest } from '../utils/auth'
 import checkJwt from '../utils/auth'
->>>>>>> dev
 
 const router = express.Router()
 
@@ -24,15 +21,12 @@ router.get('/friends/:friendId', async (req, res) => {
   // TODO: add new routes file for user information rendering this obsolete
 
   try {
-<<<<<<< HEAD
-    const wishlists = await getUserFriendsWishlists(auth0_id)
-    console.log(wishlists)
+    const friendsWishlists = await getUserFriendsWishlists(auth0_id)
+    console.log(friendsWishlists)
     //deconstructs the body of the response and
-=======
     const friendId = req.params.friendId
     const { auth0_id: authId } = await getAuthId(friendId)
     const wishlists = await getUserFriendsWishlist(authId)
->>>>>>> dev
 
     res.json(wishlists)
   } catch (err) {
@@ -41,7 +35,6 @@ router.get('/friends/:friendId', async (req, res) => {
   }
 })
 
-<<<<<<< HEAD
 // POST / api / v1 / wishlists / add
 
 router.post('/add', async (req, res) => {
@@ -59,18 +52,16 @@ router.post('/add', async (req, res) => {
 })
 
 // PUT /api/v1/wishlists / edit
-router.put('/wishlist/:wishlistId', async (req, res) => {
+router.patch('/edit/:wishlistId', async (req, res) => {
   try {
     const { wishlistId } = req.params
     const updatedData = req.body
-    const updateWishlist = await editWishlist(wishlistId, updatedData)
-    res.json(updateWishlist)
+    const updatedWishlist = await editWishlist(wishlistId, updatedData)
+    res.json(updatedWishlist)
   } catch (err) {
     res
       .status(500)
       .json({ error: 'Failed to update wishlist ${error.message}' })
   }
 })
-=======
 export default router
->>>>>>> dev
